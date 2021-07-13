4ca9f38ba01774a2b4cbfd7909ac76352c586fd0
aldolushkja <aldo.lushkja@gmail.com>
2021-07-13 22:13:25 +0200

    setting up a simple API route

---
+++ cmd/api/movieHandlers.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"aldolushkja/go-movies-be/v2/models"
+	"encoding/json"
+	"errors"
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+func (app *application) getOneMovie(w http.ResponseWriter, r *http.Request) {
+
+	params := httprouter.ParamsFromContext(r.Context())
+
+	id, err := strconv.Atoi(params.ByName("id"))
+	if err != nil {
+		app.logger.Println(errors.New("invalid id parameter"))
+	}
+
+	app.logger.Println("id is", id)
+	movie := models.Movie{
+		ID:          id,
+		Title:       "Some Movie",
+		Description: "Some Description",
+		Year:        2021,
+		ReleaseDate: time.Date(2021, 01, 01, 01, 0, 0, 0, time.Local),
+		Runtime:     100,
+		Rating:      5,
+		MPAARating:  "PG-13",
+		CreatedAt:   time.Now(),
+		UpdatedAt:   time.Now()}
+	//currentStatus := AppStatus{
+	//	Status:      "Available",
+	//	Environment: app.config.env,
+	//	Version:     version,
+	//}
+	//app.logger.Println("movie title is", movie.Title)
+	err = app.writeJSON(w, http.StatusOK, movie, "movie")
+	//if err != nil {
+	//	app.logger.Println(err)
+	//}
+	//w.Header().Set("Content-Type", "application/json")
+	//w.WriteHeader(http.StatusOK)
+	//w.Write(js)
+}
+
+func (app *application) getAllMovies(w http.ResponseWriter, r *http.Request) {
+	currentStatus := AppStatus{
+		Status:      "Available",
+		Environment: app.config.env,
+		Version:     version,
+	}
+
+	js, err := json.MarshalIndent(currentStatus, "", "\t")
+	if err != nil {
+		app.logger.Println(err)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(js)
+}
