5ca6b0330735d8411e9f40d83cb87ed6dc93727c
aldolushkja <aldo.lushkja@gmail.com>
2021-07-15 23:19:39 +0200

    setting up a CORS middleware

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -5,12 +5,12 @@ import (
 	"net/http"
 )
 
-func (app *application) routes() *httprouter.Router {
+func (app *application) routes() http.Handler {
 	router := httprouter.New()
 
 	router.HandlerFunc(http.MethodGet, "/status", app.statusHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.getOneMovie)
 	router.HandlerFunc(http.MethodGet, "/v1/movies", app.getAllMovies)
 
-	return router
+	return app.enableCORS(router)
 }
