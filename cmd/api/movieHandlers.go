665bd5c4f9c5163565134aee2ae66349c3b394f2
aldolushkja <aldo.lushkja@gmail.com>
2021-07-17 15:19:08 +0200

    creating mock for posting form data of the movie

--- cmd/api/movieHandlers.go
+++ cmd/api/movieHandlers.go
@@ -75,13 +75,24 @@ func (app *application) getAllMoviesByGenre(w http.ResponseWriter, r *http.Reque
 	}
 }
 
+func (app *application) editMovie(w http.ResponseWriter, r *http.Request) {
+	type jsonResponse struct {
+		OK bool `json:"ok"`
+	}
+
+	ok := jsonResponse{
+		OK: true,
+	}
+
+	err := app.writeJSON(w, http.StatusOK, ok, "response")
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+}
+
 func (app *application) deleteMovie(w http.ResponseWriter, r *http.Request) {
-}
 
-func (app *application) insertMovie(w http.ResponseWriter, r *http.Request) {
-}
-
-func (app *application) updateMovie(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) searchMovies(w http.ResponseWriter, r *http.Request) {
