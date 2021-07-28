cf9df4085fe8796ee3d854dc36f61bd99eb2d7de
aldolushkja <aldo.lushkja@gmail.com>
2021-07-28 23:48:27 +0200

    fix launch configuration

--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
 #! /bin/bash
+export GO_MOVIES_JWT=cccf6f0334130a7010d62332c75b53e7d8cea715e52692b06e9cd41b05644be3
+export GO_MOVIES_DSN=postgres://postgres:postgres@localhost:5432/go_movies?sslmode=disable
+
 go run cmd/api/*.go
